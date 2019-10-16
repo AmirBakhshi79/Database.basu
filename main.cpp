f12e57b96c5d083e2f1b6834ba0404270f840383
Bakhshi <amirbakhshizade79@gmail.com>
2019-10-16 14:41:55 +0330

    added some functions e.g isorisnt , Class_name , RemoveClass

--- main.cpp
+++ main.cpp
@@ -5,10 +5,38 @@
 #include <iomanip>
 using namespace std;
 void start();
+void RemoveClass(string File);
+void AddClass(string File);
 void ChangeUpperToLower(string  &name);
 bool FileName(string &pointer);
+bool Class_Name(string &pointer);
 void HelpDocumention();
+bool IsOrIsnt(string &Str);
+void RemoveStudent(unsigned long long int id);
+‫‪void AddStudent();‪
 vector <string> VectorSplitedStrings;
+struct Date
+{
+    unsigned short int Year;
+    unsigned short int Month;
+    unsigned short int Day;
+};
+struct Student
+{
+    string Firstname;
+    string Lastname;
+    unsigned long long int ID;
+    Date Birthday;
+    float Grade;
+};
+struct Class
+{
+    string ClassName;
+    float Average;
+    unsigned short int Capacity;
+    vector <Student> Data;
+};
+vector <Class> Database;
 int main()
 {
     start();
@@ -37,44 +65,47 @@ void start()
                 VectorSplitedStrings.push_back(SplitedWithSpaces);
             }
         }
-        string * PointerToVector = VectorSplitedStrings.data();
-        if(PointerToVector[0] == "basu" && PointerToVector[1] == "add" && PointerToVector[2] == "class" && FileName(PointerToVector[3]))
+        if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "add" && VectorSplitedStrings.at(2) == "class" && FileName(VectorSplitedStrings.at(3)))
         {
-            cout << "Yes";
+            AddClass(VectorSplitedStrings.at(3));
             VectorSplitedStrings.clear();
         }
-        else if(PointerToVector[0] == "basu" && PointerToVector[1] == "remove" && PointerToVector[2] == "class" && FileName(PointerToVector[3]))
+        else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "remove" && VectorSplitedStrings.at(2) == "class" && Class_Name(VectorSplitedStrings.at(3)))
         {
-
+            RemoveClass(VectorSplitedStrings.at(3));
+            VectorSplitedStrings.clear();
 
 
         }
-        else if(PointerToVector[0] == "basu" && PointerToVector[1] == "show" && FileName(PointerToVector[2]))
+        else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "show" && FileName(VectorSplitedStrings.at(2)))
         {
         cout << "dorost kar mikone :D";
         }
-        else if(PointerToVector[0] == "basu" && PointerToVector[1] == "sort" && PointerToVector[2] == "name")
+        else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "sort" && VectorSplitedStrings.at(2) == "name")
         {
             cout << "Sort Name";
         }
-        else if(PointerToVector[0] == "basu" && PointerToVector[1] == "sort" && PointerToVector[2] == "ID")
+        else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "sort" && VectorSplitedStrings.at(2) == "ID")
         {
             cout << "Sort ID";
         }
-        else if(PointerToVector[0] == "basu" && PointerToVector[1] == "save")
+        else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "save")
         {
             cout << "Saved :D";
         }
-        else if(PointerToVector[0] == "basu" && PointerToVector[1] == "help")
+        else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "help")
         {
             HelpDocumention();
+            VectorSplitedStrings.clear();
         }
-        else if(PointerToVector[0] == "basu" && PointerToVector[1] == "select" && PointerToVector[2] == "class" && FileName(PointerToVector[3]))
+        else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "select" && VectorSplitedStrings.at(2) == "class" && Class_Name(VectorSplitedStrings.at(3)))
         {
-            string YourClass = PointerToVector[3];
+            cout << VectorSplitedStrings.at(3) << " >> ";
+            string YourClass = VectorSplitedStrings.at(3);
             VectorSplitedStrings.clear();
             while(1)
             {
+
                 string YourCommandLine;
                 getline(cin , YourCommandLine);
                 ChangeUpperToLower(YourCommandLine);
@@ -94,32 +125,42 @@ void start()
                         VectorSplitedStrings.push_back(SplitedWithSpaces);
                     }
                 }
-                string * PointerToVector = VectorSplitedStrings.data();
-                if(PointerToVector[0] == "basu" && PointerToVector[1] == "remove" && PointerToVector[2] == "student" )
+                if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "remove" && VectorSplitedStrings.at(2) == "student" )
                 {
-                    cout << "Great";
+                    if(IsOrIsnt(YourClass))
+                    {
+                        unsigned long long int id = stoll(VectorSplitedStrings.at(3));
+                        RemoveStudent(id);
+
+                    }
+                    else
+                    {
+                        cout << "Your classname isnt in database..." << endl;
+                    }
+                    VectorSplitedStrings.clear();
+
                 }
-                else if(PointerToVector[0] == "basu" && PointerToVector[1] == "add" && PointerToVector[2] == "student")
+                else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "add" && VectorSplitedStrings.at(2) == "student")
                 {
                     cout <<"Add Student Pleas";
                 }
-                else if(PointerToVector[0] == "basu" && PointerToVector[1] == "search")
+                else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "search")
                 {
 
                 }
-                else if(PointerToVector[0] == "basu" && PointerToVector[1] == "search")
+                else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "search")
                 {
 
                 }
-                else if(PointerToVector[0] == "basu" && PointerToVector[1] == "show" && PointerToVector[2] != YourClass)
+                else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "show" && VectorSplitedStrings.at(2) != YourClass)
                 {
                     cout << "ali";
                 }
-                else if(PointerToVector[0] == "basu" && PointerToVector[1] == "show" && PointerToVector[2] == YourClass)
+                else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "show" && VectorSplitedStrings.at(2) == YourClass)
                 {
                     cout << "Hello World";
                 }
-                else if(PointerToVector[0] == "basu" && PointerToVector[1] == "help")
+                else if(VectorSplitedStrings.at(0) == "basu" && VectorSplitedStrings.at(1) == "help")
                 {
                     HelpDocumention();
                 }
@@ -145,6 +186,10 @@ void ChangeUpperToLower(string &name)
 {
     for(string :: iterator it = name.begin(); it != name.end() ; it++)
     {
+        if(*it == 12)
+        {
+            continue;
+        }
         if(*it >= 65 && *it <= 90)
         {
             *it += 32;
@@ -153,7 +198,7 @@ void ChangeUpperToLower(string &name)
 }
 bool FileName(string &pointer)
 {
-    vector <string> filename {"hes" , "her" , "us"};
+    vector <string> filename {"l1.basu" , "l2.basu" , "l3.basu" , "l4.basu" , "l5.basu"};
     for(vector  <string> :: iterator it = filename.begin(); it != filename.end() ; it++)
     {
         if(*it == pointer)
@@ -181,4 +226,113 @@ void HelpDocumention()
     cout << "12 : basu sort id" << endl;
     cout << "13 : basu save" << endl;
     cout << "14 : exit" << endl;
+}
+void AddClass(string File)
+{
+    const char* fName = File.c_str();
+    Class NewClass;
+    ifstream Input;
+    Input.open(fName);
+    if(!Input)
+    {
+        cout << "Ops...Error";
+    }
+    else
+    {
+        Input >> NewClass.ClassName;
+        Input >> NewClass.Capacity;
+        for(int i = 1 ; i <= NewClass.Capacity ; i++)
+        {
+            string date ;
+            string temp[3];
+            short int counter = 0;
+            Student NewStudent;
+            Input >> NewStudent.Firstname;
+            Input >> NewStudent.Lastname;
+            Input >> date;
+            for(auto j : date)
+            {
+                if (j != '/')
+                    temp[counter] += j;
+                else
+                    counter++;
+            }
+            NewStudent.Birthday.Year = stoi(temp[0]);
+            NewStudent.Birthday.Month = stoi(temp[1]);
+            NewStudent.Birthday.Day = stoi(temp[2]);
+            Input >> NewStudent.Grade;
+            Input >> NewStudent.ID;
+            NewClass.Data.push_back(NewStudent);
+        }
+    }
+    Database.push_back(NewClass);
+    Input.close();
+}
+void RemoveClass(string File)
+{
+    bool status = false;
+    for(int i = 0 ; i < Database.size()  ; i++)
+    {
+        if(File == Database.at(i).ClassName)
+        {
+            Database.erase(Database.begin() + i);
+            status = true;
+        }
+
+    }
+    if(!status)
+    {
+        cout  << "Your classname wasnt in database";
+    }
+
+}
+bool Class_Name(string &pointer)
+{
+    vector <string> classname {"advanced_programming" ,"logic_circuits" , "discrete_mathematics" , "data_structure" , "artificial_intelligence" };
+    for(vector  <string> :: iterator it = classname.begin(); it != classname.end() ; it++)
+    {
+        if(*it == pointer)
+        {
+            char * ptr[pointer.size() + 1];
+            for(int i = 0 ; i <= pointer.size() ; i++)
+            {
+                if(i == 0 || pointer[i - 1] == '_')
+                {
+                    pointer[i] -= 32;
+                }
+                ptr[i] = &pointer[i];
+
+            }
+            return true ;
+        }
+    }
+    return false;
+
+}
+bool IsOrIsnt(string &Str)
+{
+    for(auto &p : Database)
+    {
+        if(p.ClassName == Str)
+        {
+            return true;
+        }
+    }
+    return false;
+
+}
+void RemoveStudent(unsigned long long int id)
+{
+    for(auto &t : Database)
+    {
+        for(int j = 0 ; j < Database.size() ; j++)
+        {
+            if(t.Data.at(j).ID == id)
+            {
+                t.Data.erase(t.Data.begin() + j);
+                t.Capacity--;
+            }
+        }
+    }
+
 }
